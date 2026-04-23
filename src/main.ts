aa62b54ab5719594342ce8fccd6eeebab9760fba
Guyutongxue <guyutongxue@163.com>
2026-04-23 18:08:14 +0800

    rename

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import type { IndexOutput, HeaderIndex, SymbolEntry } from "./types";
 import { loadAllTexFiles, extractHeaderSynopses } from "./latex";
-import { parseCodeblock } from "./cpp";
+import { parseCodeblock } from "./cxx";
 
 const OUTPUT_FILE = "dist/std-index.json";
 
